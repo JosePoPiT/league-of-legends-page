6c45c85876bf05b32854111a75fc11cc0b85abe9
JosePoPiT <jose.garcia@upsih.com>
2023-08-01 15:52:26 -0400

    se actualiza el llamado por id del servicio de campeones

--- src/app/campeones/services/campeones.service.ts
+++ src/app/campeones/services/campeones.service.ts
@@ -4,28 +4,23 @@ import { Observable } from 'rxjs';
 import { Campeon } from '../interfaces/campeones.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CampeonesService {
-
-  
-
-  constructor( private http: HttpClient) { }
-
-  
-  // getCampeones(): Observable<Campeon[]> {
-  //   return this.http.get<Campeon[]>( 'https://ddragon.leagueoflegends.com/cdn/11.23.1/data/es_ES/champion.json')
-  // }
+  constructor(private http: HttpClient) {}
 
   getCampeones(): Observable<Campeon[]> {
-    return this.http.get<Campeon[]>('http://ddragon.leagueoflegends.com/cdn/13.14.1/data/en_US/champion.json')
+    return this.http.get<Campeon[]>(
+      'https://ddragon.leagueoflegends.com/cdn/13.14.1/data/es_ES/champion.json'
+    );
   }
   // getCampeones2(): Observable<Datum[]> {
   //   return this.http.get<Datum[]>( 'http://ddragon.leagueoflegends.com/cdn/11.23.1/data/es_ES/champion.json')
   // }
 
-  getCampeonPorId(id : string ): Observable<any> {
-    return this.http.get<any>( `https://ddragon.leagueoflegends.com/cdn/11.23.1/data/es_ES/champion/${ id }.json`)
+  getCampeonPorId(id: string): Observable<any> {
+    return this.http.get<any>(
+    `https://ddragon.leagueoflegends.com/cdn/13.14.1/data/es_ES/champion/${id}.json`   
+    );
   }
-
 }
