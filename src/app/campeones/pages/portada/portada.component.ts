6e38ccb325afc28e83e1277f054737dd6d4c0b52
JosePoPiT <jose.garcia@upsih.com>
2023-09-11 15:45:54 -0300

    se agrega info de campeones de portada

--- src/app/campeones/pages/portada/portada.component.ts
+++ src/app/campeones/pages/portada/portada.component.ts
@@ -1,59 +1,81 @@
-import { Component, OnInit, Renderer2, ViewChild, ElementRef  } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Renderer2,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 import { CampeonesService } from '../../services/campeones.service';
 import { Campeon } from '../../interfaces/campeones.interface';
 import { animate, style, transition, trigger } from '@angular/animations';
-
 
 @Component({
   selector: 'app-portada',
   templateUrl: './portada.component.html',
   styleUrls: ['./portada.component.scss'],
-  animations: [
-    trigger('fadeIn', [
-      transition(':enter', [
-        style({ opacity: 0, transform: 'translateY(20px)' }),
-        animate('500ms {{delay}}ms ease-out', style({ opacity: 1, transform: 'none' })),
-      ], { params: { delay: '0' } })
-    ])
-  ]
 })
 export class PortadaComponent implements OnInit {
   @ViewChild('imageContainer') imageContainer: ElementRef | undefined;
+  currentChampion: Campeon | null = null;
+
   currentIndex: number = 0;
 
   campeones: Campeon[] = [];
   urlImagenes: string[] = [
-    '../../../../assets/img/imagenes-portada/ezreal-portada.png',
-    '../../../../assets/img/imagenes-portada/jayce-portada.png',
-    '../../../../assets/img/imagenes-portada/lee-sin-portada.png',
-    '../../../../assets/img/imagenes-portada/morgana-portada.png',
-    '../../../../assets/img/imagenes-portada/zed-portada.png',
+    '../../../../assets/img/imagenes-portada/Ezreal-portada.png',
+    '../../../../assets/img/imagenes-portada/Jayce-portada.png',
+    '../../../../assets/img/imagenes-portada/Lee Sin-portada.png',
+    '../../../../assets/img/imagenes-portada/Morgana-portada.png',
+    '../../../../assets/img/imagenes-portada/Zed-portada.png',
   ];
 
-  constructor(private campeonesService: CampeonesService,
-    private renderer: Renderer2) { }
+  constructor(
+    private campeonesService: CampeonesService,
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit(): void {
-    this.loadImages()
+    this.loadImages();
+    this.getCampeones();
     console.log(this.urlImagenes);
-    
+
     // this.cargarImagenes();
   }
-
-  
 
   // onImageLoad() {
   //   this.currentIndex = (this.currentIndex + 1) % this.urlImagenes.length;
   // }
 
   getCampeones() {
-    this.campeonesService.getCampeones().subscribe( (data: any ) => {
-      this.campeones = Object.values( data.data )  
-      console.log( this.campeones );
+    this.campeonesService.getCampeones().subscribe((data: any) => {
+      this.campeones = Object.values(data.data);
+      console.log(this.campeones);
+    });
+  }
 
-    })
-  };
- 
+  displayChampionInfo(imgUrl: string) {
+    const imageName = this.extractImageName(imgUrl);
+
+    if (imageName) {
+      const champion = this.campeones.find((champ) => champ.name === imageName);
+
+      if (champion) {
+        this.currentChampion = champion;
+      }
+    }
+  }
+  hideChampionInfo() {
+    this.currentChampion = null;
+  }
+
+  private extractImageName(imgUrl: string): string | null {
+    // Extract the name from the image URL. You may need to adjust this based on your URL structure.
+    const parts = imgUrl.split('/');
+    const imageNameWithExtension = parts[parts.length - 1];
+    const imageName = imageNameWithExtension.split('-')[0]; // Assuming image names are like "championName-portada.png"
+    return imageName;
+  }
+
   loadImages() {
     const firstImage = new Image();
     firstImage.src = this.urlImagenes[0];
@@ -63,21 +85,10 @@ export class PortadaComponent implements OnInit {
     };
   }
 
-  // showImage() {
-  //   const images = document.querySelectorAll('.image');
-  //   images.forEach((image, index) => {
-   
-      
-      
-  //     image.classList.remove('active');
-  //     if (index === this.currentIndex) {
-  //       image.classList.add('active');
-  //     }
-  //   });
-  // }
   showImage() {
     if (this.imageContainer && this.imageContainer.nativeElement) {
-      const images = this.imageContainer.nativeElement.querySelectorAll('.image');
+      const images =
+        this.imageContainer.nativeElement.querySelectorAll('.image');
       images.forEach((image: any, index: any) => {
         if (index === this.currentIndex) {
           this.renderer.setStyle(image, 'opacity', '1');
@@ -93,15 +104,12 @@ export class PortadaComponent implements OnInit {
     this.showImage();
   }
 
-  
-
   // cargarImagenes() {
   //   for (let i = 1; i <= 5; i++) {
   //     const imgUrl = `../../../../assets/img/imagenes-portada/${i}.png`;
   //     this.urlImagenes.push(imgUrl)
   //   }
   //   console.log(this.urlImagenes);
-    
-  // }
 
+  // }
 }
