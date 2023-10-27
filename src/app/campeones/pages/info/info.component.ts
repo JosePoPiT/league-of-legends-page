8c51d4bee0b5c1fcf12c39f54d32a8d6315275ea
JosePoPiT <josegarciama23@gmail.com>
2023-10-27 17:04:19 -0300

    se agrega imagen de mapa del juego

--- src/app/campeones/pages/info/info.component.ts
+++ src/app/campeones/pages/info/info.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
-  styleUrls: ['./info.component.css']
+  styleUrls: ['./info.component.scss']
 })
 export class InfoComponent implements OnInit {
+
+  map: string = '../../../../assets/img/imagenes-informacion/mapa.jpg'
 
   constructor() { }
 
