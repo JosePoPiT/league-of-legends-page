8cda32225a1d958598af6e5e97e8ec9cb9b5bf26
Jose <josegarciama23@gmail.com>
2025-06-25 22:58:28 -0400

    assets fix

--- src/app/campeones/pages/info/info.component.ts
+++ src/app/campeones/pages/info/info.component.ts
@@ -7,13 +7,13 @@ import { mapas } from '../../../constants/mapas'
   styleUrls: ['./info.component.scss'],
 })
 export class InfoComponent implements OnInit {
-  map: string = '../../../../assets/img/imagenes-informacion/mapa.jpg';
+  map: string = 'assets/img/imagenes-informacion/mapa.jpg';
   mapInformation: string = mapas.grieta;
-  twistedMap: string = '../../../../assets/img/imagenes-informacion/twistedMap.jpg';
+  twistedMap: string = 'assets/img/imagenes-informacion/twistedMap.jpg';
   twistedMapInfo: string = mapas.twisted;
-  magmaMap: string = '../../../../assets/img/imagenes-informacion/magmaMap.jpg';
+  magmaMap: string = 'assets/img/imagenes-informacion/magmaMap.jpg';
   magmaInfo: string = mapas.magma;
-  dominionMap: string = '../../../../assets/img/imagenes-informacion/dominion.jpg';
+  dominionMap: string = 'assets/img/imagenes-informacion/dominion.jpg';
   dominionInfo: string = mapas.dominion;
   constructor() {}
 
